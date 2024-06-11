ecd33ee1a08794f4b22828b53a5bf2f9dfd1fd1b
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-11 14:23:22 -0300

    guarda de rota

--- gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
+++ gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,28 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { Animais } from '../animais';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { Animais } from '../animais';
 import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-lista-animais',
   templateUrl: './lista-animais.component.html',
-  styleUrls: ['./lista-animais.component.css']
+  styleUrls: ['./lista-animais.component.css'],
 })
 export class ListaAnimaisComponent implements OnInit {
-
   animais!: Animais;
 
-  constructor(private usuarioService: UsuarioService,
-    private animaisService: AnimaisService
-  ) { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.usuarioService.retornaUsuario().subscribe((usuario) => {
-      const userName = usuario.name ?? '';
-      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
-        this.animais = animais;
-      });
+    this.activatedRoute.params.subscribe((param) => {
+      this.animais = this.activatedRoute.snapshot.data['animais'];
     });
   }
-
 }
