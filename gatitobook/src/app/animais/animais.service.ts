eab7ba404eec7abb94aa0e4bfbb2996e20584028
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-12 09:17:12 -0300

    Registrando o interceptor

--- gatitobook/src/app/animais/animais.service.ts
+++ gatitobook/src/app/animais/animais.service.ts
@@ -15,17 +15,11 @@ export class AnimaisService {
   constructor(private http: HttpClient, private tokenService: TokenService) { }
 
   listaDoUsuario(nomeDoUsuario:string) :Observable<Animais> {
-    const token = this.tokenService.retornaToken();
-    const headers = new HttpHeaders().append('x-access', token)
-    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
-      headers,
-    });
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`,)
   }
 
   buscarPorID(id:number) : Observable<Animal> {
-    const token = this.tokenService.retornaToken();
-    const headers = new HttpHeaders().append('x-access-token', token);
-    return this.http.get<Animal>(`${API}/photos/${id}`, { headers });
+    return this.http.get<Animal>(`${API}/photos/${id}`);
   }
 
 
