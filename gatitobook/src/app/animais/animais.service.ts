ffc9078ea5646569c6a774c4498de67f9570b34c
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-13 10:31:34 -0300

    Campo de preview

--- gatitobook/src/app/animais/animais.service.ts
+++ gatitobook/src/app/animais/animais.service.ts
@@ -1,38 +1,41 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { Animais, Animal } from './animais';
-import { TokenService } from '../autenticacao/token.service';
-import { environment } from 'src/environments/environment';
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, mapTo } from 'rxjs/operators';
-import { error } from 'console';
+import { environment } from 'src/environments/environment';
+import { TokenService } from '../autenticacao/token.service';
+import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
-const NOT_MOFIFIED = '304';
+const NOT_MODIFIED = '304';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnimaisService {
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  constructor(private http: HttpClient, private tokenService: TokenService) { }
-
-  listaDoUsuario(nomeDoUsuario:string) :Observable<Animais> {
-    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`,)
+  listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
   }
 
-  buscaPorID(id:number) : Observable<Animal> {
+  buscaPorID(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
   }
 
-  excluiAnimal(id:number) : Observable<Animal> {
+  excluiAnimal(id: number): Observable<Animal> {
     return this.http.delete<Animal>(`${API}/photos/${id}`);
   }
 
-  curtir(id:number) : Observable<boolean> {
-    return this.http.post(`${API}/photos/${id}/like`, {}, {observe: 'response'}).pipe(mapTo(true),catchError((error) => {
-      return error.status === NOT_MOFIFIED ? of(false) : throwError(error);
-    }))
+  curtir(id: number): Observable<boolean> {
+    return this.http
+      .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        })
+      );
   }
 
   upload(descricao: string, permiteComentario: boolean, arquivo: File) {
@@ -46,5 +49,4 @@ export class AnimaisService {
       reportProgress: true,
     });
   }
-
 }
